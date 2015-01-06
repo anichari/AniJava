e9bf9a4a21f47da0e7894cfd269d14e5169535e7
Anirudh Chari <anirudhmchari@gmail.com>
2015-01-06 14:16:06 -0800

    cleanup
    cleanup

--- Intro to Java Programming /lesson2/days2/DaysAlivePrinter.java
+++ Intro to Java Programming /lesson2/days2/DaysAlivePrinter.java
@@ -2,11 +2,18 @@ public class DaysAlivePrinter
 {
     public static void main(String[] args)
     {
+        // This program calculates the number of days
+        // that Sally Ride lived.
+        // Initialize birthDay variable
         Day birthDay = new Day(1951, 5, 26);
+        // Initialize lastDay variable
         Day lastDay = new Day(2012, 7, 23);
+        // Print the last day
         System.out.print("LastDay: ");
         System.out.println(lastDay.toString());
+        // Calculate the number of days Alive and assign to int
         int daysAlive = lastDay.daysFrom(birthDay);
+        // Print days alive
         System.out.print("Days alive: ");
         System.out.println(daysAlive);
     }
